e49fdb40ee3ae487a9ad37a533e8599a226a9352
timhun <tim.oneway@gmail.com>
2025-08-11 20:36:05 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
-US_TICKERS = ['^IXIC', '^GSPC', 'QQQ', 'SPY', 'BTC-USD']
+US_TICKERS = ['^IXIC', '^GSPC', 'QQQ', 'SPY', 'BTC-USD', 'GC=F']
 TW_TICKERS = ['^TWII', '0050.TW', '2330.TW']
 
 US_MARKET_NAMES = {
@@ -7,7 +7,8 @@ US_MARKET_NAMES = {
     '^GSPC': '標普500指數',
     'QQQ': 'Invesco QQQ Trust',
     'SPY': 'SPDR S&P 500 ETF',
-    'BTC-USD': '比特幣'
+    'BTC-USD': '比特幣',
+    'GC=F': '黃金期貨'
 }
 
 TW_MARKET_NAMES = {
