1114f8f68d5827d4a3e6278bae618cac1a4fb966
timhun <tim.oneway@gmail.com>
2025-08-26 22:37:47 +0900

    Create cloud_manager.py

---
+++ cloud_manager.py
@@ -0,0 +1,16 @@
+from b2sdk.v2 import InMemoryAccountInfo, B2Api
+import os
+
+def upload_episode(date, mode, files):
+    info = InMemoryAccountInfo()
+    b2_api = B2Api(info)
+    b2_api.authorize_account("production", os.getenv('B2_KEY_ID'), os.getenv('B2_APPLICATION_KEY'))
+    bucket = b2_api.get_bucket_by_name(os.getenv('B2_BUCKET_NAME'))
+
+    folder = f"episodes/{date}_{mode}/"
+    uploaded = {}
+    for file_type, file_path in files.items():
+        file_name = folder + os.path.basename(file_path)
+        bucket.upload_local_file(local_file=file_path, file_name=file_name)
+        uploaded[file_type] = f"https://f005.backblazeb2.com/file/{os.getenv('B2_BUCKET_NAME')}/{file_name}"
+    return uploaded
